2513b01df767e56e39c2d88724b5b4f35959a5a4
Murali Suriar <code@suriar.com>
2013-01-20 21:03:52 +0000

    Working both ways, I think.

--- src/roman/roman.go
+++ src/roman/roman.go
@@ -13,14 +13,31 @@ type Numeral struct {
 
 var numerals []Numeral
 
+var characters map[rune]int
+
 func init() {
+	numerals = append(numerals, Numeral{1000, string("M")})
+	numerals = append(numerals, Numeral{900, string("CM")})
+	numerals = append(numerals, Numeral{500, string("D")})
+	numerals = append(numerals, Numeral{400, string("CD")})
 	numerals = append(numerals, Numeral{100, string("C")})
 	numerals = append(numerals, Numeral{90, string("XC")})
+	numerals = append(numerals, Numeral{50, string("L")})
+	numerals = append(numerals, Numeral{40, string("XL")})
 	numerals = append(numerals, Numeral{10, string("X")})
 	numerals = append(numerals, Numeral{9, "IX"})
 	numerals = append(numerals, Numeral{5, string("V")})
 	numerals = append(numerals, Numeral{4, "IV"})
 	numerals = append(numerals, Numeral{1, string("I")})
+
+  characters = make(map[rune]int)
+  characters['I'] = 1
+  characters['V'] = 5
+  characters['X'] = 10
+  characters['L'] = 50
+  characters['C'] = 100
+  characters['D'] = 500
+  characters['M'] = 1000
 }
 
 func DecimalToRoman(x int) (s string) {
@@ -35,4 +52,23 @@ func DecimalToRoman(x int) (s string) {
 	}
 	s = strings.Join(outputs, "")
 	return
+}
+
+func RomanToDecimal(s string) (x int) {
+  x = 0
+  for i := 0 ; i < len(s) ; i++ {
+    current,next := characters[rune(s[i])],0
+
+    if i+1 < len(s) {
+      next = characters[rune(s[i+1])]
+    }
+
+    if current < next {
+      x += next - current
+      i++
+    } else {
+      x += current
+    }
+  }
+  return x
 }
--- src/roman/roman_test.go
+++ src/roman/roman_test.go
@@ -2,8 +2,10 @@ package roman
 
 import "testing"
 
-func TestDecimalToRoman(t *testing.T) {
-	tests := map[int]string{
+var tests map[int]string
+
+func init() {
+	tests = map[int]string{
 		1:   string('I'),
 		3:   "III",
 		4:   "IV",
@@ -19,7 +21,20 @@ func TestDecimalToRoman(t *testing.T) {
 		20:  "XX",
 		90:  "XC",
 		100: "C",
+    1900: "MCM",
+    1990: "MCMXC",
 	}
+}
+
+func TestRomanToDecimal(t *testing.T) {
+	for out, in := range tests {
+		if x := RomanToDecimal(in); x != out {
+			t.Errorf("RomanToDecimal(%v) = %v, want %v", in, x, out)
+		}
+	}
+}
+
+func TestDecimalToRoman(t *testing.T) {
 	for in, out := range tests {
 		if x := DecimalToRoman(in); x != out {
 			t.Errorf("DecimalToRoman(%v) = %v, want %v", in, x, out)
