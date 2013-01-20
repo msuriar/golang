8733e899b12a60df4b36a514fe1007270c98afd6
Murali Suriar <code@suriar.com>
2013-01-20 19:12:11 +0000

    Gofmt.

--- src/roman/roman.go
+++ src/roman/roman.go
@@ -7,29 +7,31 @@ import (
 // import "fmt"
 
 type Numeral struct {
-  dec int
-  rom string
+	dec int
+	rom string
 }
 
 var numerals []Numeral
 
 func init() {
-  numerals = append(numerals, Numeral{10, string("X")})
-  numerals = append(numerals, Numeral{9, "IX"})
-  numerals = append(numerals, Numeral{5, string("V")})
-  numerals = append(numerals, Numeral{4, "IV"})
-  numerals = append(numerals, Numeral{1, string("I")})
+	numerals = append(numerals, Numeral{100, string("C")})
+	numerals = append(numerals, Numeral{90, string("XC")})
+	numerals = append(numerals, Numeral{10, string("X")})
+	numerals = append(numerals, Numeral{9, "IX"})
+	numerals = append(numerals, Numeral{5, string("V")})
+	numerals = append(numerals, Numeral{4, "IV"})
+	numerals = append(numerals, Numeral{1, string("I")})
 }
 
 func DecimalToRoman(x int) (s string) {
 	var outputs []string
 	for x > 0 {
-    for _,r := range(numerals) {
-      for x >= r.dec {
-        outputs = append(outputs, r.rom)
-        x -= r.dec 
-      }
-    }
+		for _, r := range numerals {
+			for x >= r.dec {
+				outputs = append(outputs, r.rom)
+				x -= r.dec
+			}
+		}
 	}
 	s = strings.Join(outputs, "")
 	return
--- src/roman/roman_test.go
+++ src/roman/roman_test.go
@@ -3,18 +3,26 @@ package roman
 import "testing"
 
 func TestDecimalToRoman(t *testing.T) {
-  tests := map[int]string{
-    1: string('I'),
-    3: "III",
-    4: "IV",
-    5: string('V'),
-    8: "VIII",
-    9: "IX",
-    10: "X",
-  }
-  for in,out := range tests {
-    if x:= DecimalToRoman(in); x != out {
-      t.Errorf("DecimalToRoman(%v) = %v, want %v", in, x, out)
-    }
-}
+	tests := map[int]string{
+		1:   string('I'),
+		3:   "III",
+		4:   "IV",
+		5:   string('V'),
+		8:   "VIII",
+		9:   "IX",
+		10:  "X",
+		13:  "XIII",
+		14:  "XIV",
+		15:  "XV",
+		16:  "XVI",
+		19:  "XIX",
+		20:  "XX",
+		90:  "XC",
+		100: "C",
+	}
+	for in, out := range tests {
+		if x := DecimalToRoman(in); x != out {
+			t.Errorf("DecimalToRoman(%v) = %v, want %v", in, x, out)
+		}
+	}
 }
