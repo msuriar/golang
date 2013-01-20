2dec2c21ca9eab127c7bae1a9365cd8531fd7fe0
Murali Suriar <code@suriar.com>
2013-01-20 19:07:31 +0000

    Genericised.

--- src/roman/roman.go
+++ src/roman/roman.go
@@ -6,26 +6,30 @@ import (
 
 // import "fmt"
 
+type Numeral struct {
+  dec int
+  rom string
+}
+
+var numerals []Numeral
+
+func init() {
+  numerals = append(numerals, Numeral{10, string("X")})
+  numerals = append(numerals, Numeral{9, "IX"})
+  numerals = append(numerals, Numeral{5, string("V")})
+  numerals = append(numerals, Numeral{4, "IV"})
+  numerals = append(numerals, Numeral{1, string("I")})
+}
+
 func DecimalToRoman(x int) (s string) {
 	var outputs []string
 	for x > 0 {
-		switch {
-    case x >= 10:
-      outputs = append(outputs, string("X"))
-      x -= 10
-    case x >= 9:
-      outputs = append(outputs, string("IX"))
-      x -= 9
-		case x >= 5:
-			outputs = append(outputs, string("V"))
-			x -= 5
-		case x >= 4:
-			outputs = append(outputs, string("IV"))
-			x -= 4
-		default:
-			outputs = append(outputs, string("I"))
-			x--
-		}
+    for _,r := range(numerals) {
+      for x >= r.dec {
+        outputs = append(outputs, r.rom)
+        x -= r.dec 
+      }
+    }
 	}
 	s = strings.Join(outputs, "")
 	return
